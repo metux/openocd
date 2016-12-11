25fd380cb8729365e1f8dcec4ddf7d4429b6fcd1
barthess <barthess@yandex.ru>
2016-12-11 15:11:41 +0300

    XCF (Xilinx platfrom flash) support.
    Change-Id: I4ee6db5f0abdb9fd279cc0edd13f71952a9d295d
    Signed-off-by: Uladzimir Pylinski <barthess@yandex.ru>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -66,6 +66,7 @@ extern struct flash_driver str9x_flash;
 extern struct flash_driver str9xpec_flash;
 extern struct flash_driver tms470_flash;
 extern struct flash_driver virtual_flash;
+extern struct flash_driver xcf_flash;
 extern struct flash_driver xmc1xxx_flash;
 extern struct flash_driver xmc4xxx_flash;
 
@@ -120,6 +121,7 @@ static struct flash_driver *flash_drivers[] = {
 	&str9xpec_flash,
 	&tms470_flash,
 	&virtual_flash,
+	&xcf_flash,
 	&xmc1xxx_flash,
 	&xmc4xxx_flash,
 	NULL,
